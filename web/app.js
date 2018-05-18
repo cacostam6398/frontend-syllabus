6a8fd6e010431040e737778eebf413f45cef8c7c
cacostam6398 <36524775+cacostam6398@users.noreply.github.com>
2018-05-18 15:56:07 -0500

    cambio estructura
    adelanto wizard

--- web/app.js
+++ web/app.js
@@ -2,7 +2,20 @@
 
 var IdentiApp = angular.module('Identi', ['ngRoute', 'ui.bootstrap', 'ui.tree', 'ngAnimate', 'ui.bootstrap.datetimepicker', 'ngIdle']);
 
-IdentiApp.run(function ($rootScope, $location, $http) {
+IdentiApp.constant('CONFIG', {   
+    ROL_CURRENT_USER: 1
+   })
+
+   .constant('ROLES', {
+    PROFESOR:  ['/home', '/CreacionSyllabus']        
+}
+)
+
+
+
+IdentiApp.run(function ($rootScope, $location, $http, CONFIG, ROLES) {
+    console.log('permisos + rol')
+    console.log(ROLES)
     var storage;
     try {
         if (sessionStorage.getItem('user')) {
@@ -18,6 +31,22 @@ IdentiApp.run(function ($rootScope, $location, $http) {
         storage = {};
     }
 
+    $rootScope.$on('$routeChangeStart', function (event, next) 
+    {
+                    console.log(next.$$route.originalPath)
+                if (next.data !== undefined) 
+                {
+                    
+                    next.data.authorized.forEach(function (elemento, indice, array) {
+                        console.log(elemento, indice);
+                    });
+
+                    // if(next.data.authorized.indexOf(CONFIG.ROL_CURRENT_USER) !== -1)
+                    //     {
+                    //     console.log("entra");
+                    //     }  
+                }
+    });
 
     $rootScope.$on('$routeChangeSuccess', function () {
 
@@ -40,13 +69,15 @@ IdentiApp.run(function ($rootScope, $location, $http) {
 })
 
 
-IdentiApp.config(['$routeProvider', '$locationProvider', '$httpProvider', 'IdleProvider', '$controllerProvider', '$provide',
-	function ($routeProvider, $locationProvider, $httpProvider, IdleProvider, $controllerProvider, $provide) {
+IdentiApp.config(['$routeProvider', '$locationProvider', '$httpProvider', 'IdleProvider', '$controllerProvider', '$provide','CONFIG', 'ROLES',
+	function ($routeProvider, $locationProvider, $httpProvider, IdleProvider, $controllerProvider, $provide,CONFIG, ROLES) {
 	    $routeProvider
-        .when('/home', { controller: 'HomeController', controllerAs: 'HmeCtrl', templateUrl: 'partials/home.html' })
+        .when('/home', { controller: 'HomeController', controllerAs: 'HmeCtrl', templateUrl: 'partials/home.html' , data: { authorized: [ROLES.PROFESOR]}})
         .when('/login', { controller: 'LoginController', templateUrl: 'partials/login.html' })
         .when('/detalleSyllabus', { controller: 'HistoriaSyllabusController', controllerAs: 'HstSCtrl', templateUrl: 'partials/historiaSyllabus.html' })
-        .when('/CreacionSyllabus', { controller: 'WizardCreacionSyllabus', controllerAs: 'WizCtrl', templateUrl: 'partials/wizardCreacionSyllabus.html' })
+        .when('/CreacionSyllabus', { controller: 'WizardCreacionSyllabus', controllerAs: 'WizCtrl', templateUrl: 'partials/wizardCreacionSyllabus.html',data: {
+                                     authorized: [ROLES.PROFESOR]}
+             })
         
 
 	    
