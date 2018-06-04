f43dcaba227b03b5748228b6f09c8a086785b529
cdacosta8 <acosta.david09@gmail.com>
2018-06-04 17:22:49 -0500

    llenar combos servicios

--- web/js/controllers/wizardCreacionSyllabus.js
+++ web/js/controllers/wizardCreacionSyllabus.js
@@ -42,6 +42,7 @@ IdentiApp.controller("WizardCreacionSyllabus", ['Enviar', 'Cargar', '$location',
     this.currentDate = new Date();
     this.currentYear =  this.currentDate.getFullYear();
     $rootScope.dataSyllabus={}
+    this.contCreate1 = true;
 
     var e = jQuery.Event("keypress");
     e.which = 13; //choose the one you want
@@ -96,7 +97,7 @@ IdentiApp.controller("WizardCreacionSyllabus", ['Enviar', 'Cargar', '$location',
         var Ctrl = this;
         var success = function (json) {
             Ctrl.ListMatProgramas = json.data.programas;
-            console.log(Ctrl.ListMatProgramas)
+            // console.log(Ctrl.ListMatProgramas)
         };
         var error = function (resp) { console.log("Error: " + resp); jQuery(".progress").hide(); };        
         Enviar.elemento(Ctrl, url, success, error, jsonEnvio);
@@ -106,17 +107,19 @@ IdentiApp.controller("WizardCreacionSyllabus", ['Enviar', 'Cargar', '$location',
 
 
     this.ObtProgramasByPrograma = function(idPrograma){
-
-        
         var Ctrl = this;
+           $rootScope.dataSyllabus.cabecera.codigoMateria = ''
+            $rootScope.dataSyllabus.cabecera.facultad = ''
+            $rootScope.dataSyllabus.cabecera.creditos = ''
+            $rootScope.dataSyllabus.cabecera.tipo = ''
+            $rootScope.dataSyllabus.cabecera.descripcionMateria = ''
+            $rootScope.dataSyllabus.cabecera.materia = ""
+        
         Ctrl.ListMatSelect = this.ListMatProgramas.filter(function(mat){
-
             
-            if(mat.programa.id_programa == idPrograma ){
-                console.log(mat.programa.id_programa);
+            if(mat.programa.id_programa == idPrograma ){                
                 return true; 
-            }else{
-               
+            }else{               
                 return false;
             }
 
@@ -127,6 +130,72 @@ IdentiApp.controller("WizardCreacionSyllabus", ['Enviar', 'Cargar', '$location',
          
       
     }
+
+
+    this.llenarCamposCabecera = function(idMateria){
+        var Ctrl = this;
+        let idPrograma = $rootScope.dataSyllabus.cabecera.programa;
+        let idMaterial = idMateria ;
+        
+        if(idPrograma != "" && idMaterial != "" ){            
+            var jsonPrograma = Ctrl.ListMatSelect[0].programa;
+            var jsonMateria =  Ctrl.ListMatSelect[0].materias.filter(function(mat){
+                if(mat.id_materia == idMateria ){                
+                    return true; 
+                }else{                   
+                    return false;
+                }       
+            });
+
+            $rootScope.dataSyllabus.cabecera.codigoMateria = jsonMateria[0].codigo
+            $rootScope.dataSyllabus.cabecera.facultad = jsonPrograma.facultad
+            $rootScope.dataSyllabus.cabecera.creditos = jsonMateria[0].creditos
+            $rootScope.dataSyllabus.cabecera.tipo = jsonMateria[0].tipo
+            $rootScope.dataSyllabus.cabecera.descripcionMateria = jsonMateria[0].descripcion
+
+        }else{
+
+            $rootScope.dataSyllabus.cabecera.codigoMateria = ''
+            $rootScope.dataSyllabus.cabecera.facultad = ''
+            $rootScope.dataSyllabus.cabecera.creditos = ''
+            $rootScope.dataSyllabus.cabecera.tipo = ''
+            $rootScope.dataSyllabus.cabecera.descripcionMateria = ''
+            $rootScope.dataSyllabus.cabecera.materia = ""
+        }
+
+        
+
+    }
+
+    this.CrearCabeceraSyllabus = function(){
+        var Ctrl = this;
+        let data =  $rootScope.dataSyllabus.cabecera
+        if( typeof data != 'undefined' ){
+            if(data.materia != "" && data.programa != ""  ){  
+            
+                var jsonEnvio =  { 'id_usuario': $rootScope.user.id_usuario, 'token': $rootScope.token, "id_materia":data.materia, "correo": $rootScope.user.correo,"observacion":"P"}
+                var url =  $rootScope.baseUri + "/restapi-syllabusean/public/syllabus/crearsyl";
+                
+                var success = function (json) {
+                    swal("info", 'cabecera creada', "success");                  
+                    Ctrl.contCreate1 = false;
+                    console.log(json)
+                };
+                var error = function (resp) { console.log("Error: " + resp);Ctrl.contCreate1 = true; swal("info", 'Error en el servicio', "info")};        
+                Enviar.elemento(Ctrl, url, success, error, jsonEnvio);
+            }else{
+                Ctrl.contCreate1 = true;
+                swal("info", 'Completar informacion', "info");
+
+            }
+        }else{
+            Ctrl.contCreate1 = true;
+            swal("info", 'Completar informacion', "info");
+
+        }
+    }
+
+    
 
     this.cargarCombos(); 
     funcionalidadWiz();
